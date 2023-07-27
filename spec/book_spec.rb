01cac221f3ff3e0ae084167805c487461e5e8d8d
Lane Bretschneider <lanebret85@gmail.com>
2023-07-27 13:41:48 -0700

    test: add tests for Book class

---
+++ spec/book_spec.rb
@@ -0,0 +1,26 @@
+require './lib/book'
+
+RSpec.describe Book do
+  describe "#initializes" do
+    it "exists" do
+      book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+
+      expect(book).to be_a(Book)
+    end
+
+    it "has readable attributes" do
+      book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+
+      expect(book.title).to eq("To Kill a Mockingbird")
+      expect(book.author).to eq("Harper Lee")
+    end
+  end
+
+  describe "#publication_year" do
+    it "can return publication year from publication_date argument" do
+      book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+      
+      expect(book.publication_year).to eq("1960")
+    end
+  end
+end
