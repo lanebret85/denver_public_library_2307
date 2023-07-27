9b03ffa63d271258f433d25fa2469b5c037d660b
Lane Bretschneider <lanebret85@gmail.com>
2023-07-27 15:46:44 -0700

    feat: add publication_time_frame_for method

--- lib/library.rb
+++ lib/library.rb
@@ -6,11 +6,16 @@ class Library
   def initialize(name)
     @name = name
     @books = []
-    @authors = []
+    @authors = [] 
   end
 
   def add_author(author)
     @authors << author
     author.books.map { |book| @books << book }
+  end
+
+  def publication_time_frame_for(author)
+    pub_years = author.books.map { |book| book.publication_year }
+    pub_time_frame = {start: pub_years.sort.first, end: pub_years.sort.last}
   end
 end
